3fc7f0919a50a8edd94e7654369428fa878c52cb
Jeremy Jordan <>
2020-09-13 15:29:40 +0000

    logging with wandb

--- flower_classifier/models/train.py
+++ flower_classifier/models/train.py
@@ -8,21 +8,40 @@ class FlowerClassifier(pl.LightningModule):
         super().__init__()
         self.network = network
         self.criterion = nn.CrossEntropyLoss()
+        self.accuracy_metric = pl.metrics.Accuracy()
+
+    @property
+    def example_input_array(self):
+        return torch.zeros(1, 3, 256, 256)
 
     def forward(self, x):
         return self.network(x)
 
     def _step(self, batch):
         imgs, labels = batch
-        outputs = self(imgs)
-        loss = self.criterion(outputs, labels)
-        return {"loss": loss}
+        logits = self(imgs)
+        loss = self.criterion(logits, labels)
+        preds = nn.functional.softmax(logits, dim=1).argmax(1)
+        acc = self.accuracy_metric(preds, labels)
+        return {"loss": loss, "accuracy": acc}
 
     def training_step(self, batch, batch_idx):
-        return self._step(batch)
+        step_result = self._step(batch)
+        loss = step_result["loss"]
+        acc = step_result["accuracy"]
+        result = pl.TrainResult(minimize=loss)
+        result.log("train/loss", loss)
+        result.log("train/acc", acc)
+        return result
 
     def validation_step(self, batch, batch_idx):
-        return self._step(batch)
+        step_result = self._step(batch)
+        loss = step_result["loss"]
+        acc = step_result["accuracy"]
+        result = pl.EvalResult()
+        result.log("val/loss", loss)
+        result.log("val/acc", acc)
+        return result
 
     def configure_optimizers(self):
         return torch.optim.Adam(self.parameters(), lr=1e-3)
@@ -30,14 +49,16 @@ class FlowerClassifier(pl.LightningModule):
 
 if __name__ == "__main__":
     from pytorch_lightning import Trainer
+    from pytorch_lightning.loggers import WandbLogger
 
     from flower_classifier.datasets.oxford_flowers import (
         OxfordFlowersDataModule,
     )
     from flower_classifier.models.baseline import BaselineResnet
 
-    trainer = Trainer()
     network = BaselineResnet()
     model = FlowerClassifier(network=network)
-    data_module = OxfordFlowersDataModule()
-    trainer.fit(model, data_module)
+    data_module = OxfordFlowersDataModule(batch_size=32)
+    logger = WandbLogger(project="test", tags=["test"])
+    trainer = Trainer(logger=logger, overfit_pct=0.1, checkpoint_callback=False, row_log_interval=1)
+    trainer.fit(model, datamodule=data_module)
