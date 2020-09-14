f305f56527aa5e281fc20fc37d901815c5b054f0
Jeremy Jordan <13970565+jeremyjordan@users.noreply.github.com>
2020-09-14 18:02:53 -0400

    logging with wandb (#12)
    * logging with wandb
    
    * updated settings
    
    * add checkpointing
    
    Co-authored-by: Jeremy Jordan <>

--- flower_classifier/models/train.py
+++ flower_classifier/models/train.py
@@ -4,40 +4,64 @@ import torch.nn as nn
 
 
 class FlowerClassifier(pl.LightningModule):
-    def __init__(self, network):
+    def __init__(self, network, learning_rate=1e-3):
         super().__init__()
+        self.save_hyperparameters("learning_rate")
         self.network = network
         self.criterion = nn.CrossEntropyLoss()
+        self.accuracy_metric = pl.metrics.Accuracy()
+
+    @property
+    def example_input_array(self):
+        return torch.zeros(1, 3, 256, 256)
 
     def forward(self, x):
         return self.network(x)
 
     def _step(self, batch):
         imgs, labels = batch
-        outputs = self(imgs)
-        loss = self.criterion(outputs, labels)
-        return {"loss": loss}
+        logits = self(imgs)
+        loss = self.criterion(logits, labels)
+        preds = nn.functional.softmax(logits, dim=1).argmax(1)
+        acc = self.accuracy_metric(preds, labels)
+        return {"loss": loss, "accuracy": acc}
 
     def training_step(self, batch, batch_idx):
-        return self._step(batch)
+        step_result = self._step(batch)
+        loss = step_result["loss"]
+        acc = step_result["accuracy"]
+        result = pl.TrainResult(minimize=loss)
+        result.log("train/loss", loss)
+        result.log("train/acc", acc)
+        return result
 
     def validation_step(self, batch, batch_idx):
-        return self._step(batch)
+        step_result = self._step(batch)
+        loss = step_result["loss"]
+        acc = step_result["accuracy"]
+        result = pl.EvalResult(checkpoint_on=loss)
+        result.log("val/loss", loss)
+        result.log("val/acc", acc)
+        return result
 
     def configure_optimizers(self):
-        return torch.optim.Adam(self.parameters(), lr=1e-3)
+        return torch.optim.Adam(self.parameters(), lr=self.hparams.learning_rate)
 
 
 if __name__ == "__main__":
     from pytorch_lightning import Trainer
+    from pytorch_lightning.callbacks import ModelCheckpoint
+    from pytorch_lightning.loggers import WandbLogger
 
     from flower_classifier.datasets.oxford_flowers import (
         OxfordFlowersDataModule,
     )
     from flower_classifier.models.baseline import BaselineResnet
 
-    trainer = Trainer()
     network = BaselineResnet()
-    model = FlowerClassifier(network=network)
-    data_module = OxfordFlowersDataModule()
-    trainer.fit(model, data_module)
+    model = FlowerClassifier(network=network, learning_rate=0.01)
+    data_module = OxfordFlowersDataModule(batch_size=32)
+    logger = WandbLogger(project="flowers", tags=["oxford102"])
+    checkpoint_callback = ModelCheckpoint(monitor="val/loss", save_top_k=3)
+    trainer = Trainer(gpus=1, logger=logger, row_log_interval=1, callbacks=[checkpoint_callback])
+    trainer.fit(model, datamodule=data_module)
