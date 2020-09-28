57a7127c6f781f638c27fd2579707d7c63a19345
Jeremy Jordan <jtjordan@ncsu.edu>
2020-09-27 22:57:47 -0400

    enhancements to streamlit

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 import tempfile
 
+import plotly.express as px
 import streamlit as st
 import torch.nn as nn
 import torchvision
@@ -8,7 +9,7 @@ from PIL import Image
 
 from flower_classifier.datasets.oxford_flowers import NAMES as oxford_idx_to_names
 from flower_classifier.models.baseline import BaselineResnet
-from flower_classifier.models.train import FlowerClassifier
+from flower_classifier.models.classifier import FlowerClassifier
 
 st.set_option("deprecation.showfileUploaderEncoding", False)
 
@@ -45,15 +46,24 @@ if checkpoint_file:
     st.write("Model is ready to make predictions!")
 
 photo_bytes = st.file_uploader("Input image:", ["png", "jpg", "jpeg"])
-if photo_bytes:
-    pil_image = Image.open(photo_bytes).convert("RGB")
-    st.image(pil_image, caption="Input image", width=480)
-    inputs = torchvision.transforms.Compose(
-        [
-            torchvision.transforms.ToTensor(),
-            torchvision.transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
-        ]
-    )(pil_image)
-    logits = model(inputs.unsqueeze(0))
-    pred = nn.functional.softmax(logits, dim=1).argmax(1).item()
-    st.write(f"Prediction: {oxford_idx_to_names[pred]}")
+if not photo_bytes:
+    st.stop()
+
+pil_image = Image.open(photo_bytes).convert("RGB")
+st.image(pil_image, caption="Input image", use_column_width=True)
+inputs = torchvision.transforms.Compose(
+    [
+        torchvision.transforms.ToTensor(),
+        torchvision.transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+    ]
+)(pil_image)
+logits = model(inputs.unsqueeze(0))
+pred = nn.functional.softmax(logits, dim=1)
+top_pred = pred.max(1)
+label = oxford_idx_to_names[top_pred.indices.item()]
+score = top_pred.values.item()
+st.write(f"Prediction: {label}")
+st.write(f"Score: {score:0.4}")
+
+pred_dist = px.bar(y=pred.squeeze().detach().numpy(), x=oxford_idx_to_names, title="Prediction Distribution")
+st.plotly_chart(pred_dist)
