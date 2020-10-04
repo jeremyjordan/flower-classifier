eff19c5dd996a14925dcbf4c45b2cfdaed9f7c47
Jeremy Jordan <>
2020-10-04 01:46:29 +0000

    create backbones using timm

--- flower_classifier/cli.py
+++ flower_classifier/cli.py
@@ -9,7 +9,6 @@ from pytorch_lightning.loggers import WandbLogger
 
 from flower_classifier import ROOT_DATA_DIR
 from flower_classifier.datasets.oxford_flowers import OxfordFlowersDataModule
-from flower_classifier.models.baseline import BaselineResnet
 from flower_classifier.models.classifier import FlowerClassifier
 
 app = typer.Typer()
@@ -17,14 +16,21 @@ app = typer.Typer()
 
 @app.command()
 def train(
-    learning_rate: float = 0.01,
+    architecture: str = "resnet34",
+    dropout_rate: float = 0.0,
+    global_pool: str = "avg",
+    learning_rate: float = 1e-3,
     batch_size: int = 32,
     max_epochs: int = 1000,
     gpu: bool = False,
     data_dir: str = ROOT_DATA_DIR,
 ):
-    network = BaselineResnet()
-    model = FlowerClassifier(network=network, learning_rate=learning_rate)
+    model = FlowerClassifier(
+        architecture=architecture,
+        learning_rate=learning_rate,
+        dropout_rate=dropout_rate,
+        global_pool=global_pool,
+    )
     data_module = OxfordFlowersDataModule(batch_size=batch_size, data_dir=data_dir)
     logger = WandbLogger(project="flowers", tags=["oxford102"])
     checkpoint_callback = ModelCheckpoint(save_top_k=3, filepath=logger.experiment.dir)
