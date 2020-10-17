898eb61fa4b6fcffb40200a559eb77744a5e34d0
Jeremy Jordan <jtjordan@ncsu.edu>
2020-10-17 17:10:05 -0400

    remove unneeded arg

--- tests/models/test_hydra.py
+++ tests/models/test_hydra.py
@@ -5,7 +5,7 @@ from flower_classifier.train import train
 
 def test_hydra_config(tmpdir, trainer="test", dataset="random"):
     cmd_line = "trainer={} dataset={} hydra.run.dir={}"
-    with initialize(config_path="../../conf", job_name="cofig"):
+    with initialize(config_path="../../conf"):
         cfg = compose(
             config_name="config",
             overrides=cmd_line.format(trainer, dataset, tmpdir).split(" "),
