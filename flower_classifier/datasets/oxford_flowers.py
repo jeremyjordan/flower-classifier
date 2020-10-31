6ac0945343884fb88eae1de4f228ec2267c199f3
Jeremy Jordan <jtjordan@ncsu.edu>
2020-10-30 23:16:11 -0400

    remove default transforms in code

--- flower_classifier/datasets/oxford_flowers.py
+++ flower_classifier/datasets/oxford_flowers.py
@@ -122,12 +122,6 @@ NAMES = [
 IMAGE_URL = "https://www.robots.ox.ac.uk/~vgg/data/flowers/102/102flowers.tgz"
 LABELS_URL = "https://www.robots.ox.ac.uk/~vgg/data/flowers/102/imagelabels.mat"
 
-DEFAULT_IMG_TRANSFORMS = [
-    torchvision.transforms.RandomResizedCrop(224),
-    torchvision.transforms.ToTensor(),
-    torchvision.transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
-]
-
 
 def download_dataset(root_dir: str):
     root_dir = Path(root_dir)
@@ -180,7 +174,7 @@ class OxfordFlowers102Dataset(Dataset):
 
 
 class OxfordFlowersDataModule(pl.LightningDataModule):
-    def __init__(self, data_dir=ROOT_DATA_DIR, batch_size=64, transforms=DEFAULT_IMG_TRANSFORMS):
+    def __init__(self, data_dir=ROOT_DATA_DIR, batch_size=64, transforms=[]):
         super().__init__()
         self.data_dir = data_dir
         self.batch_size = batch_size
