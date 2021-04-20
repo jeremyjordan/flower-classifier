b5a05e4dbe209cde6d30cc51c6117a29ba471e68
Jeremy Jordan <jtjordan@ncsu.edu>
2021-04-19 22:49:48 -0400

    collect user feedback

--- app.py
+++ app.py
@@ -14,10 +14,9 @@ pil_image = components.get_photo()
 st.image(pil_image, caption="Input image", use_column_width=True)
 preds = components.make_prediction(model, pil_image)
 
-components.display_prediction(preds)
+predicted_class = components.display_prediction(preds)
 components.display_top_3_table(preds)
 components.display_prediction_distribution(preds)
-
 
 with st.beta_expander("Supported flower breeds"):
     breeds = "The model can recognize the following breeds:"
@@ -29,14 +28,28 @@ with st.beta_expander("Supported flower breeds"):
 st.markdown(
     """
     Want to help us improve this model? Share the photo with us so we can include it
-    in our training data!
+    in our training data! Just click the button labeled "Save photo to database".
     """
 )
-save_photo = st.button("Save photo to database")
+tags = set()
+model_pred = predicted_class.replace(" ", "_")
+model_pred_tag = f"pred:{model_pred}"
+tags.add(model_pred_tag)
 
+
+# Does the user think the prediction is correct?
+is_correct_tag = components.ask_user_if_correct()
+tags.add(is_correct_tag)
+
+# Does the user know what breed the flower actually is?
+if is_correct_tag == "user_judgement:wrong":
+    guess_breed_tag = components.ask_user_for_breed()
+    tags.add(guess_breed_tag)
+
+save_photo = st.button("Save photo to database")
 if save_photo:
     flickr_client = get_authenticated_client()
     image_hash = hashlib.md5(pil_image.tobytes()).hexdigest()
-    upload_photo(flickr_client, filename=f"{image_hash}.jpg", pil_image=pil_image)
+    upload_photo(flickr_client, filename=f"{image_hash}.jpg", pil_image=pil_image, tags=tags)
     st.balloons()
     st.success("Thanks for sharing!")
