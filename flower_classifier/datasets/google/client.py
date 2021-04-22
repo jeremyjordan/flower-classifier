80e99a1a107e9bec59a970a7737ec383eab35fb7
John Huffman <huffmanjohnf@gmail.com>
2021-04-21 22:03:12 -0400

    Update flower_classifier/datasets/google/client.py
    Co-authored-by: Jeremy Jordan <13970565+jeremyjordan@users.noreply.github.com>

--- flower_classifier/datasets/google/client.py
+++ flower_classifier/datasets/google/client.py
@@ -20,5 +20,5 @@ def get_secrets():
 def query_google_images(flower_name: str):
     secrets = get_secrets()
     gis = GoogleImagesSearch(secrets["GIS_API_KEY"], secrets["GIS_PROJECT_CX"])
-    gis.search({"q": f"{prediction_name} flower", "num": 3})
+    gis.search({"q": f"{flower_name} flower", "num": 3})
     return [result.url for result in gis.results()]
